dc81320b2b826db5ed2361c82bdb3381467e2737
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-03-24 18:55:55 -0300

    Create ongsController and its index and store methods

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
 
+const ongsController = require('./controllers/ongsController');
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+routes.get('/ongs', ongsController.index);
+routes.post('/ongs', ongsController.store);
 
 module.exports = routes;
--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const routes = require('./routes');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
