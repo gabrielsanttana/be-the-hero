35058f28a82e47225f1d036312f1c9ad21f63cc6
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-03-24 18:15:05 -0300

    Create knex database connection file

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-lfmbb.mongodb.net/be-the-hero`, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true
-});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
