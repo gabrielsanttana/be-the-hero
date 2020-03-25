50ed0d85b45f726d99d0387e2959e4efd8e76727
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-24 21:39:12 -0300

    Add IncidentController GET and POST routes

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,10 +1,13 @@
 const {Router} = require('express');
 
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
 
 module.exports = routes;
