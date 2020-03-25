e61944e068f237fb6591cd833a1635c401e686cc
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-03-25 19:14:19 -0300

    Add ProfileController index method

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.post('/ongs', OngController.store);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.store);
 routes.delete('/incidents:id', IncidentController.delete);
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
