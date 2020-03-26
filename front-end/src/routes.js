1eee3a74323c971bb2487d63b2c0007ff01f22e6
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-25 22:15:03 -0300

    Create routes file

---
+++ front-end/src/routes.js
@@ -0,0 +1,13 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
