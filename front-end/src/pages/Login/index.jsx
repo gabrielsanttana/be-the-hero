9292d1c990c96e6e7d1faf23decce4722f2171cc
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-26 20:10:15 -0300

    Create login page structure

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
+
+import logo from '../../assets/logo.svg';
+import heroes from '../../assets/heroes.png';
 
 import './styles.css';
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container">
+      <section className="form">
+        <img src={logo} alt="logo"/>
+
+        <form>
+          <h1>Faça seu login</h1>
+
+          <input type="text" name="" id=""/>
+          <button type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn
+              size={16}
+              color="#e02041" 
+            /> 
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+
+      <img src={heroes} alt="heroes"/>
     </div>
   );
 }
