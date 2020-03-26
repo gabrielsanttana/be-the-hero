b666f9bcbc761a50c818fbe24eb5614e7f993a1b
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-25 21:57:51 -0300

    Install and config CORS

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
 app.use(routes);
