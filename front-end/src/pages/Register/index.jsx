fef1256e7b7066b2d980218ff81273d9d4f8192d
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-27 20:19:34 -0300

    Add register page styles

--- front-end/src/pages/Register/index.jsx
+++ front-end/src/pages/Register/index.jsx
@@ -9,51 +9,38 @@ import './styles.css';
 function Register() {
   return (
     <div className="register-container">
-      <section className="title">
-        <img src={logo} alt="logo" />
+      <div className="content">
+        <section className="title">
+          <img src={logo} alt="logo" />
 
-        <h1>Cadastro</h1>
+          <h1>Cadastro</h1>
 
-        <p>
-          Faça seu cadastro, entre na plataforma e ajude pessoas
-          a encontrarem os casos da sua ONG.
-        </p>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude pessoas
+            a encontrarem os casos da sua ONG.
+          </p>
 
-        <Link>
-          <FiArrowLeft 
-            size={46}
-            color="#e02041"
-          />
-          Voltar para o login
-        </Link>
-      </section>
+          <Link to="/">
+            <FiArrowLeft size={16} color="#e02041" />
+            Voltar para o login
+          </Link>
+        </section>
 
-      <form>
-        <input 
-          type="text"
-          placeholder="Nome da ONG"
-          autoFocus
-        />
-        <input 
-          type="email"
-          placeholder="E-mail"
-        />
-        <input 
-          type="text"
-          placeholder="Whatsapp"
-        />
-        <input 
-          type="text"
-          placeholder="Cidade"
-        />
-        <input 
-          type="text"
-          placeholder="UF"
-        />
-        <button type="submit">
-          Cadastrar
-        </button>
-      </form>
+        <form>
+          <input type="text" placeholder="Nome da ONG" autoFocus />
+          <input type="email" placeholder="E-mail" />
+          <input type="text" placeholder="Whatsapp" />
+
+          <div className="input-group">
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" style={{width: 80}} />
+          </div>
+
+          <button type="submit" className="button">
+            Cadastrar
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
