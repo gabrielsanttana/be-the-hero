d5acfa10451903c51364ffdf37af184b59f840b4
gabrielgsa <gabriel.gsantana7@gmail.com>
2020-03-26 21:00:44 -0300

    Add '/register' route

--- front-end/src/pages/Register/index.jsx
+++ front-end/src/pages/Register/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './styles.css';
+
 function Register() {
   return (
     <div>
--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Login} />
+        <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
   );
