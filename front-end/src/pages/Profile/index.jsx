e9f63aaf86c348f4fc23881e31d778a812c61a17
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-03-28 17:58:39 -0300

    Add API call in profile page and make it dynamic

--- front-end/src/pages/Profile/index.jsx
+++ front-end/src/pages/Profile/index.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import {FiPower, FiTrash2} from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import './styles.css';
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const ongID = localStorage.getItem('ongID');
   const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    api.get('/profile', {
+      headers: {
+        Authorization: ongID,
+      }
+    }).then(response => {
+      setIncidents(response.data);
+    });
+  }, [ongID]);
 
   return (
     <div className="profile-container">
@@ -25,79 +40,29 @@ export default function Profile() {
 
       <div className="content">
         <h1>Casos cadastrados</h1>
-
+      
         <ul>
-          <li>
-            <strong>CASO:</strong>
+          {incidents.map(({title, description, value}) => (
+            <li>
+              <strong>CASO:</strong>
 
-            <p>Cadelinha atropelada</p>
+              <p>{title}</p>
 
-            <strong>DESCRIÇÃO:</strong>
-            
-            <p>
-              A cadelinha Jolie foi atropelada por um carro no bairro
-              Santana e teve que passar por uma cirurgia às pressas.
-            </p>
+              <strong>DESCRIÇÃO:</strong>
 
-            <strong>VALOR:</strong>
+              <p>
+                {description}
+              </p>
 
-            <p>R$120,00 reais</p>
+              <strong>VALOR:</strong>
 
-            <button>
-              <FiTrash2 size={20} color="#a8a8b3" />
-            </button>
-          </li>
+              <p>{`R$${value}`}</p>
 
-          <li>
-            <strong>CASO:</strong>
-
-            <p>Cadelinha atropelada</p>
-
-            <strong>DESCRIÇÃO:</strong>
-            
-            <p>
-              A cadelinha Jolie foi atropelada por um carro no bairro
-              Santana e teve que passar por uma cirurgia às pressas.
-            </p>
-
-            <strong>VALOR:</strong>
-
-            <p>R$120,00 reais</p>
-          </li>
-
-          <li>
-            <strong>CASO:</strong>
-
-            <p>Cadelinha atropelada</p>
-
-            <strong>DESCRIÇÃO:</strong>
-            
-            <p>
-              A cadelinha Jolie foi atropelada por um carro no bairro
-              Santana e teve que passar por uma cirurgia às pressas.
-            </p>
-
-            <strong>VALOR:</strong>
-
-            <p>R$120,00 reais</p>
-          </li>
-
-          <li>
-            <strong>CASO:</strong>
-
-            <p>Cadelinha atropelada</p>
-
-            <strong>DESCRIÇÃO:</strong>
-            
-            <p>
-              A cadelinha Jolie foi atropelada por um carro no bairro
-              Santana e teve que passar por uma cirurgia às pressas.
-            </p>
-
-            <strong>VALOR:</strong>
-
-            <p>R$120,00 reais</p>
-          </li>
+              <button>
+                <FiTrash2 size={20} color="#a8a8b3" />
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
