d9e687aea9deb2ec98e7c437e1b10ce9ae29b0d0
Gabriel Santana <gabriel.gsantana7@gmail.com>
2020-03-29 15:57:20 -0300

    Add details page sendEmail function

--- mobile/src/pages/Details/index.js
+++ mobile/src/pages/Details/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {View, Text, TouchableOpacity, Image} from 'react-native';
 import {useNavigation} from '@react-navigation/native'
 import {Feather} from '@expo/vector-icons';
+import * as MailComposer from 'expo-mail-composer';
 
 import logo from '../../assets/logo.png';
 
@@ -10,8 +11,18 @@ import styles from './styles';
 function Details() {
   const navigation = useNavigation();
 
+  const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no "Cadelinha atropelada" no valor de R$120,00';
+
   function navigateBack() {
     navigation.goBack();
+  }
+
+  function sendEmail() {
+    MailComposer.composeAsync({
+      subject: 'Herói do caso: Cadelinha atropelada',
+      recipients: ['gabrie.gsantana7@gmail.com'],
+      body: message,
+    });
   }
 
   return (
@@ -49,7 +60,9 @@ function Details() {
 
         <View style={styles.actions}>
           <TouchableOpacity style={styles.action}><Text style={styles.actionText}>Whatsapp</Text></TouchableOpacity>
-          <TouchableOpacity style={styles.action}><Text style={styles.actionText}>E-mail</Text></TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={sendEmail}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
